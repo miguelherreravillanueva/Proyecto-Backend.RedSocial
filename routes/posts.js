ffc641f64258d6c531d5e4bbbc660660ac149ff2
Miguel <mirkouda@gmail.com>
2022-11-10 18:02:42 +0100

    comment at posts.js added

--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,7 @@ const router = express.Router()
 router.post("/createPost",PostController.createPost)
 router.put("/updatePostById/:_id",PostController.updatePostById)
 router.delete("/deletePostById/:_id",PostController.deletePostById)
-router.get("/getPostByTitle/:title",PostController.getPostByTitle)
+router.get("/getPostByTitle/:title",PostController.getPostByTitle) //esta me da tira error pero en la DB de Mongo 
 router.get("/getPostById/:_id",PostController.getPostById)
 
 
