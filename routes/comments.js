ed14dd0715056600d4b809154c686cb250876bbf
Miguel <mirkouda@gmail.com>
2022-11-16 13:57:10 +0100

    Delete Comment added

--- routes/comments.js
+++ routes/comments.js
@@ -7,6 +7,6 @@ const router = express.Router()
 router.post("/createComment/:_id",authentication,CommentController.createComment)
 router.put("/updateCommentById/:_id",authentication,CommentController.updateCommentById)
 router.get("/getComments",CommentController.getComments) 
-// router.delete("/deleteComentById/:_id",authentication,CommentController.deletePostById)
+router.delete("/deleteCommentById/:_id",authentication,CommentController.deleteCommentById)
 
 module.exports = router
