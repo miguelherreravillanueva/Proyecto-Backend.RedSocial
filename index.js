dbcc19aff673cf19f8b64e82d70022a2395b5872
Miguel <mirkouda@gmail.com>
2022-12-13 16:29:42 +0100

    PORT changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const swaggerUI= require("swagger-ui-express")
 const docs = require("./docs/index")
 const { typeError }= require('./middlewares/errors');
