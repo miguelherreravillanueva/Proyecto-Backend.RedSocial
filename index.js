dde546655104fcc3dda28080cdf59bd725751ea6
Miguel <mirkouda@gmail.com>
2022-12-14 11:34:06 +0100

    Cors installed

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 const swaggerUI= require("swagger-ui-express")
 const docs = require("./docs/index")
 const { typeError }= require('./middlewares/errors');
 const { dbConnection } = require("./config/config")
+const cors = require ("cors")
+
 
 app.use(express.json())
 dbConnection()
+app.use(cors())
 
 app.use('/posts', require('./routes/posts'));
 app.use("/users", require('./routes/users'))
